a3b96f58981fbe2f413a71f7a1ef8c2922813fa7
Rodrigo D’Agostino <rodrigo.dagostino@proton.me>
2024-03-05 14:34:57 -0300

    feat: Migrate to Next 14

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
-const path = require('path')
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
-  },
-}
+const nextConfig = {};
 
-module.exports = nextConfig
+export default nextConfig;
